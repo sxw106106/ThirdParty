a90e02bdc75e7651e04d89a58fc85c4f1b4d9718
suxiaowen1 <suxiaowen1@jd.com>
2019-08-20 11:08:41 +0800

    add makefile dir

---
+++ makefile/def.h
@@ -0,0 +1,9 @@
+#ifndef __DEF_H__
+#define __DEF_H__
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+#endif
---
+++ makefile/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "def.h"
+int main()
+{
+	int i = add(1, 2);
+	printf("i = %d\n", i);
+	return 0;
+}
